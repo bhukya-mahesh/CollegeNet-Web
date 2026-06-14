52c0fa8f9875e7d89296ccc92441a1d99f1f6d87
bhukya-mahesh <bhukyamahesh4546@gmail.com>
2026-06-14 12:07:01 +0530

    Add Vercel SPA rewrite

--- backend/server.js
+++ backend/server.js
@@ -120,9 +120,20 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
 
 app.get("/",(req,res)=> res.send("Hello World"));
