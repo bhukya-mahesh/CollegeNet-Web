6bc7792f63bcce147cbe5911623e2a1bf0572374
bhukya-mahesh <bhukyamahesh4546@gmail.com>
2026-06-17 11:47:44 +0530

    mail check

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -10,6 +10,8 @@ const  transporter = nodemailer.createTransport({
      }
  
 });
+  console.log("SMTP USER:", process.env.SMPT_USER);
+  console.log("SMTP PASS EXISTS:", !!process.env.SMPT_PASS);
 
 transporter.verify(function (error, success) {
   if (error) {
